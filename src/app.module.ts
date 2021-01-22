337955e2c8020f990f36ead3703bd7f799bdb1eb
johnyworld <johnyworld@naver.com>
2021-01-22 23:00:47 +0900

    Database Configuration: Introducing ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot({
-    autoSchemaFile: true,
-  }), RestaurantsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'gimjaehwan',
+      password: '',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }), 
+    RestaurantsModule,
+  ],
   controllers: [],
   providers: [],
 })
