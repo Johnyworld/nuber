dde5ac384bfd380c6ff505b746353c9ec9824c83
johnyworld <johnyworld@naver.com>
2021-01-28 23:00:21 +0900

    TypeORM and Nest: Mapped Types

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,6 +1,8 @@
-import { Field, ObjectType } from "@nestjs/graphql";
+import { Field, InputType, ObjectType } from "@nestjs/graphql";
+import { IsBoolean, IsString, Length } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@InputType({ isAbstract: true })
 @ObjectType()
 @Entity()
 export class Restaurant {
@@ -11,22 +13,28 @@ export class Restaurant {
 
   @Field(type => String)
   @Column()
+  @IsString()
+  @Length(5)
   name: string;
 
   @Field(type => Boolean, {nullable: true})
   @Column()
+  @IsBoolean()
   isVegan?: boolean;
 
   @Field(type => String)
   @Column()
+  @IsString()
   adress: string;
 
   @Field(type => String)
   @Column()
+  @IsString()
   ownerName: string;
 
   @Field(type => String)
   @Column()
+  @IsString()
   categoryName: string;
 
 }
